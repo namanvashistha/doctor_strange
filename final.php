e0e723e612bdf0de1473acdfa837d467bcd6fc62
namanvashistha <namanvashistha15@gmail.com>
2018-10-27 17:30:17 +0530

    updated file names

---
+++ final.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel =stylesheet type=text/css href="final.css">
+</head>
+<body>
+<div>
+	naman
+</div>
+
+
+    <div id="form1" >
+	<form >
+		<textarea placeholder="Update this post" rows="4" cols="100" ></textarea>
+		<input type="submit" id="btn_signin" name="posting">
+	</form>
+	</div>
+</body>
+</html>
